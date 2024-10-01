76ee73b0f261451cc9167707171905f1786a7a1f
GiedreKite <giedre.narvilaite@gmail.com>
2024-10-02 01:54:43 +0300

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,104 @@
+import React, { useContext } from 'react';
+
+import { LanguageContext } from '../language/LanguageContext'; 
+import free from './img/free.jpg';
+import freedom from './img/freedom.jpg';
+import freee from './img/freee.jpg';
+import careme from './img/careme.jpg';
+import freetime from './img/freetime.jpg';
+import fre from './img/fre.jpg';
+import I from './img/I.jpg';
+import moletai from './img/moletai.jpg';
+import bum from './img/bum.jpg';
 
 import Header from '../header/Header';
+import style from './About.module.css';
+import Photo from '../photo/Photo';
 
 
 export default function About() {
+const { language, translations } = useContext(LanguageContext); 
+
 
 return(   <>
 <Header/>
-   <h2>Giedrė Narvilaitė: About</h2>         
+<h2 className={style.text}>{translations[language].aboutMe}</h2>  
+<div className={style.container}>
+     
+  <div className={style.img}>
+  <Photo src={moletai} alt="My Photo" />
+  </div>
+  <div className={style.text}>
+  <p>{translations[language].aboutMe1}</p>  
+   </div>
+
+<div className={style.img}>
+<Photo src={I} alt="My Photo" />
+</div>
+</div>
+<div className={style.container}>
+
+<div className={style.text}>
+<p>{translations[language].aboutMe2}</p>  
+</div>
+<div className={style.img}>
+<Photo src={careme} alt="My Photo" />
+</div>
+
+<div className={style.text}>
+<p>{translations[language].aboutMe3}</p>  
+</div>
+</div>
+<div className={style.container}>
+<div className={style.img}>
+<Photo src={freetime} alt="My Photo" />
+</div>
+
+<div className={style.text}>
+  <p>{translations[language].aboutMe4}</p>  
+</div>
+<div className={style.img}>
+<Photo src={fre} alt="My Photo" />
+</div>
+</div>
+<div className={style.container}>
+<div className={style.text}>
+<p>{translations[language].aboutMe5}</p>  
+</div>
+<div className={style.img}>
+<Photo src={freedom} alt="My Photo" />
+</div>
+
+<div className={style.text}>
+<p>{translations[language].aboutMe6}</p>  
+</div>
+</div>
+<div className={style.container}>
+<div className={style.img}>
+<Photo src={freee} alt="My Photo" />
+</div>
+
+<div className={style.text}>
+<p>{translations[language].aboutMe7}</p>  
+</div>
+<div className={style.img}>
+<Photo src={bum} alt="My Photo" />
+</div>
+</div>
+<div className={style.container}>
+<div className={style.text}>
+<p>{translations[language].aboutMe8}</p>  
+
+</div>
+
+
+
+
+
+
+
+
+</div>
 
 </>
                 
--- src/components/language/translations.js
+++ src/components/language/translations.js
@@ -19,6 +19,16 @@ const translations = {
       description: "to my creative world! Every project here is part of my journey, and I'm glad you're a part of it now.",
       name: 'I am - ',
 
+      aboutMe: 'I would like to introduce myself briefly to make our cooperation smoother and more efficient.',
+      aboutMe1: "Hello, I was born and raised in the port city of Lithuania, Klaipėda, in a beautiful and large family.",
+      aboutMe2: ' I am the eldest and the only girl in the family, so I developed caring, empathy and the ability to help others from an early age.',
+      aboutMe3: ' My life philosophy is based on openness - I believe that anything is possible if you look at the world with wide eyes and reach your goals with confidence.',
+      aboutMe4: ' I like to look at life with humor, consider failures as valuable lessons and not be afraid to make mistakes, and learn from mistakes. ',
+      aboutMe5: 'Since sport has always been an integral part of my family, it has become a way for me to breathe, let off steam and overcome difficulties. ',
+      aboutMe6: 'Extreme sports are especially close to me because they help me relax and regain energy. ',
+      aboutMe7: 'I sincerely believe that I can help others to move forward, overcome challenges and reach heights together.',
+      aboutMe8: 'I hope my introduction helped us get to know each other better and will encourage further cooperation.',
+      
       edu1: 'I got some experience with: HTML, CSS, Javascript, ReactJs, ExpressJs and MySQL.',
       edu2: 'I acquired a solid foundation in financial analysis and learned to use professional programs effectively. Thanks to these skills, I successfully secured a job that aligns with my field of expertise.',
       edu3: 'I studied at a prestigious high school, where high-level teachers work, helping to develop strong, responsible and well-prepared students.',
@@ -47,6 +57,16 @@ const translations = {
       welcome: "Sveiki atvykę",
       description: "į mano kūrybinį pasaulį! Kiekvienas projektas čia yra dalis mano kelionės, ir aš džiaugiuosi, kad dabar esate jos dalis.",
       name: 'Aš esu - ',
+
+      aboutMe: 'Norėčiau trumpai prisistatyti, kad mūsų bendradarbiavimas būtų sklandesnis ir efektyvesnis.',
+      aboutMe1: 'Sveiki, gimiau ir užaugau Lietuvos uostamiestyje, Klaipėdoje, gražioje ir didelėje šeimoje.',
+      aboutMe2: 'Esu vyriausia ir vienintelė mergaitė šeimoje, todėl nuo mažens išsiugdžiau rūpestingumą, empatiją ir gebėjimą padėti kitiems.',
+      aboutMe3: 'Mano gyvenimo filosofija paremta atvirumu – tikiu, kad žvelgiant į pasaulį plačiomis akimis ir siekiant tikslų su pasitikėjimu, viskas yra įmanoma.',
+      aboutMe4: 'Mėgstu į gyvenimą žvelgti su humoru, nesėkmes laikyti vertingomis pamokomis ir nebijoti klysti, o iš klaidų – mokytis.',
+      aboutMe5: 'Kadangi sportas visada buvo neatsiejama mano šeimos dalis, jis man tapo būdu atsikvėpti, išsikrauti ir įveikti sunkumus.',
+      aboutMe6: 'Ekstremalus sportas man ypač artimas, nes jis padeda atsipalaiduoti ir atgauti energiją.',
+      aboutMe7: 'Nuoširdžiai tikiu, kad galiu padėti ir kitiems žengti į priekį, įveikti iššūkius ir drauge siekti aukštumų.',
+      aboutMe8: 'Tikiuosi, mano prisistatymas padėjo mums geriau vieniems kitus pažinti ir skatins tolesnį bendradarbiavimą.',
 
       edu1: 'Įgavau patirties su: HTML, CSS, Javascript, ReactJs, ExpressJs ir MySQL.',
       edu2: 'Įgijau tvirtus finansų analizės pagrindus ir išmokau efektyviai naudotis profesinėmis programomis. Šių įgūdžių dėka sėkmingai radau darbą, atitinkantį mano specialybę.',
